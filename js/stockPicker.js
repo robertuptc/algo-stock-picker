2488295a3060ae75952df97e7d351f5ee4e54ece
robertuptc <robertuptc@gmail.com>
2022-06-07 01:34:16 -0400

    finished exercise

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,18 @@
-exports.picker = function(prices) {
-
+exports.picker = function (prices) {
+    let arr = []
+    let count = 0
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[i] < prices[j]) {
+                if (prices[j] - prices[i] > count) {
+                    count = prices[j] - prices[i]
+                    arr.pop()
+                    arr.push([i, j])
+                }
+            } else {
+                continue
+            }
+        }
+    } return arr.flat()
 }
+
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,21 @@
+import itertools
+
 def picker(prices):
-    pass
+    arr =[[0]]
+    count = 0
+
+    for idx1, value1 in enumerate(prices):
+        for idx2 in range(1, len(prices), 1):
+            # print(idx2)
+            if value1 < prices[idx2]:
+
+                if prices[idx2] - value1 > count and idx1 < idx2:                    
+                    count = prices[idx2] - value1
+                    # print(arr)
+                    arr.pop(0)
+                    # print(arr)
+                    arr.append([idx1, idx2])
+                    # print(arr)
+            else:
+                continue 
+    return list(itertools.chain(*arr))
